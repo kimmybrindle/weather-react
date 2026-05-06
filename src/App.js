452e8c1f37ec1ec6b50d124d29df7bbe37abce72
kimmybrindle <kimmybrindle@gmail.com>
2026-05-06 16:16:46 -0700

    added github link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ function App() {
         <h1>Weather App</h1>
         <Weather />
       </header>
+      <footer>
+        This code is{" "}
+        <a href="https://github.com/kimmybrindle/weather-react">
+          open-sourced on Github
+        </a>{" "}
+        by Kimmy Brindle.
+      </footer>
     </div>
   );
 }
